97dfb4ab5107a90bc965f709b6405f9af1085e45
Peter <m4inly@web.de>
2019-02-10 09:52:57 +0100

    add initial spring boot application

---
+++ src/main/java/de/fail/test/failsafetest/FailsafeTestApplication.java
@@ -0,0 +1,14 @@
+package de.fail.test.failsafetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailsafeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FailsafeTestApplication.class, args);
+	}
+
+}
+
